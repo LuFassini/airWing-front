74f34810656291b839b5faf7f9bd18e7b6ff78fd
LuFassini <123502647+LuFassini@users.noreply.github.com>
2024-05-27 09:02:28 -0300

    Pacotes

---
+++ src/screens/Pacotes/styles.js
@@ -0,0 +1,63 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    logo: {
+        width: 100,
+        height: 100     
+    },
+    container: {
+        backgroundColor: '#fff',
+        width: '100%',
+        height: '100%',
+    },
+    quadro: {
+        backgroundColor: 'rgba(203, 115, 228, 0.6)',
+        padding: 20,
+        margin: 20,
+        borderRadius: 5,
+    },
+    titulo: {
+        color: '#000',
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold', 
+    },
+    texto: {
+        color: '#000',
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    quadro2: {
+        marginTop: -35,
+        backgroundColor: 'rgba(0, 181, 242, 0.3)',
+        padding: 20,
+        margin: 20,
+        borderRadius: 5,
+        marginTop : -23
+    },
+    image: {
+        width: 300,
+        height: 300,
+        margin: 20,
+        borderRadius: 10,
+        marginLeft: 50,
+    },
+    quadro3: {
+        backgroundColor: 'rgba(104, 221, 189, 0.48)',
+        padding: 20,
+        margin: 20,
+        borderRadius: 10,
+    },
+    quadro4: {
+        backgroundColor: 'rgba(104, 221, 189)',
+        padding: 20,
+        margin: 20,
+        borderRadius: 10,
+        marginTop: -35,
+        padding: 20,
+        height: 100,
+    },
+});
+
+export default styles;
+
