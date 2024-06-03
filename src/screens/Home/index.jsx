76e54d0dee27e546f26088749ea77408cce2efc0
IsaBarquilia <isabelle.barquilia.564@gmail.com>
2024-06-03 14:57:36 -0300

    feat: Add search inputs and button to home screen

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,4 +1,4 @@
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, TextInput, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
 import Title from "../../components/Title";
@@ -11,11 +11,41 @@ import NewFooter from "../../components/NewFooter";
 export default function Home() {
   return (
     <ScrollView>
-    <View style={styles.container}>
-    <EasyNavegation />
-       
-    <NewFooter />
-    </View>
+      <View style={styles.container}>
+        <EasyNavegation />
+
+                <View style={styles.Inputs}>
+                    <TextInput style={styles.input}
+                        placeholder="Para onde você vai?"
+                        onChangeText={text => onChangeText(text)}
+                    />
+                    </View>
+                     <View style={styles.InputsMenor}>
+                    <TextInput style={styles.inputM}
+                        placeholder="Data de entrada"
+                        onChangeText={text => onChangeText(text)}
+                    />
+
+                    <TextInput style={styles.inputM2}
+                        placeholder="Data de saída"
+                        onChangeText={text => onChangeText(text)}
+                    />  
+                      </View>
+                      <TextInput style={styles.input}
+                        placeholder="Quartos e Hóspedes"
+                        onChangeText={text => onChangeText(text)}
+                    />
+                <TouchableOpacity style={styles.inputBuscar}>
+                    <Text style={styles.textoBotao}>Buscar Hotéis!</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.subTitulo}>Hotéis</Text>
+                <Text style={styles.linha}>________________________________________________</Text>
+
+
+        <NewFooter />
+      </View>
     </ScrollView>
+
   );
 }
