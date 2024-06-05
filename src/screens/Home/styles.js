3ba31e24c6025564aba381c9e3519d717844918b
IsaBarquilia <isabelle.barquilia.564@gmail.com>
2024-06-05 09:34:03 -0300

    ajustes hoteis

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,3 +1,4 @@
+import { height } from "@fortawesome/free-brands-svg-icons/fa42Group";
 import { StyleSheet } from "react-native-web";
 
 const styles = StyleSheet.create({
@@ -7,7 +8,39 @@ const styles = StyleSheet.create({
         height: '100%',
         flex: 1,
     },
-
+    negresco: {
+        backgroundColor: 'rgba(203, 115, 228, 0.45)',
+        padding: 20,
+        margin: 20,
+        borderRadius: 5,
+    },
+    madrid: {
+        backgroundColor: 'rgba(203, 115, 228, 0.45)',
+        padding: 20,
+        margin: 20,
+        borderRadius: 5,
+    },
+    anantara: {
+        backgroundColor: 'rgba(203, 115, 228, 0.45)',
+        padding: 20,
+        margin: 20,
+        borderRadius: 5,
+    },
+    nomeHotel: {
+        color: '#A000EB',
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    localizacao: {
+        color: '#130799',
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    preco: {
+        color: '#130799',
+        fontSize: 16,
+        textAlign: 'center',
+    },
     quadro: {
         backgroundColor: 'rgba(203, 115, 228, 0.45)',
         padding: 20,
@@ -19,7 +52,7 @@ const styles = StyleSheet.create({
         padding: 20,
         margin: 20,
         borderRadius: 5,
-        marginTop : -10
+        marginTop: -10
     },
     titulo: {
         color: '#A000EB',
@@ -29,66 +62,71 @@ const styles = StyleSheet.create({
     input: {
         width: 370,
         marginLeft: 22,
-        height: 50, 
+        height: 50,
         marginTop: 10,
         borderRadius: 2,
         borderColor: '#00B5F2',
         borderWidth: 2,
+        fontSize: 30
     },
 
     input2: {
         width: 370,
         marginLeft: 22,
-        height: 50, 
+        height: 50,
         marginTop: 8,
         borderRadius: 2,
         borderColor: '#00B5F2',
-        borderWidth: 2,
+        borderWidth: 30,
     },
     InputsMenor: {
         display: 'flex',
         flexDirection: 'row',
     },
-    Inputs:{
+    Inputs: {
         marginTop: 40
     },
     inputM: {
         width: 180,
         marginLeft: 22,
-        height: 50, 
+        height: 50,
         marginTop: 8,
         borderRadius: 2,
         borderColor: '#00B5F2',
         borderWidth: 2,
+        fontSize: 30
     },
 
     inputM2: {
         width: 180,
         marginLeft: 12,
-        height: 50, 
+        height: 50,
         marginTop: 8,
         borderRadius: 2,
         borderColor: '#00B5F2',
         borderWidth: 2,
+        fontSize: 30,
+
     },
 
     input3: {
         width: 370,
         marginLeft: 22,
-        height: 50, 
+        height: 50,
         marginTop: 10,
         borderRadius: 2,
         borderColor: '#00B5F2',
         borderWidth: 2,
+        fontSize: 25,
     },
-    inputData:{
+    inputData: {
         display: 'flex',
         flexDirection: 'row',
     },
     inputBuscar: {
         width: 370,
         marginLeft: 22,
-        height: 50, 
+        height: 50,
         marginTop: 20,
         borderRadius: 2,
         borderColor: '#00B5F2',
@@ -116,17 +154,19 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         marginLeft: 22,
         marginTop: 20,
-        marginBottom: 20,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        marginBottom: 12,
     },
     textoBotao: {
         color: '#1A6069',
+        fontSize: 25,
+        fontWeight: 'bold',
         textAlign: 'center',
         alignItems: 'center',
-        marginTop: 5,
-        fontSize: 20,
+        marginTop: 10
+
     }
 });
 
